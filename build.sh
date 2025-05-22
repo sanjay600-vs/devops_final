472d3cf74f06ec221c58ad87af887008802fc85b
sanjay600-vs <sanjayselvaraj600@gmail.com>
2025-05-22 12:06:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 docker build -t task37 .
 echo Hyyy
 docker login -u sanjayvs600-p 09-Mar-05
-docker tag task37 sanjayvs600/agri
-docker push  sanjayvs600/agri
+docker tag task37 sanjayvs600/agri1
+docker push  sanjayvs600/agri1
 kubectl apply -f  deploy.yaml --validate=false
 kubectl apply -f svc.yaml --validate=false
