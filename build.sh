821aa5d382562478a50f2292d8795a12c9de5b88
sanjay600-vs <sanjayselvaraj600@gmail.com>
2025-05-22 12:30:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker build -t task37 .
 echo Hyyy
-docker login -u sanjayvs600-p 09-Mar-05
+docker login -u sanjayvs600-p Sanjay@2005
 docker tag task37 sanjayvs600/agri1
 docker push  sanjayvs600/agri1
 kubectl apply -f  deploy.yaml --validate=false
